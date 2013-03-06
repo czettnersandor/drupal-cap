ef1cdee8d37c170e3361e76fcc7ad690fd7099dc
Sandor Czettner <sandor@healthywebsites.co.uk>
2013-03-06 13:53:26 +0000

    Remove automatic tagging

--- lib/capistrano-drupal.rb
+++ lib/capistrano-drupal.rb
@@ -22,7 +22,6 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :shared_children, ['files', 'private']
     
   after "deploy:update_code", "drupal:symlink_shared", "drush:site_offline", "drush:updatedb", "drush:cache_clear", "drush:site_online"
-  after "deploy", "git:push_deploy_tag"
   
   namespace :deploy do
     desc <<-DESC
@@ -55,23 +54,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
     end
   end
-  
-  namespace :git do
 
-    desc "Place release tag into Git and push it to origin server."
-    task :push_deploy_tag do
-      user = `git config --get user.name`
-      email = `git config --get user.email`
-      tag = "release_#{release_name}"
-      if exists?(:stage)
-        tag = "#{stage}_#{tag}"
-      end
-      puts `git tag #{tag} #{revision} -m "Deployed by #{user} <#{email}>"`
-      puts `git push origin tag #{tag}`
-    end
-
-   end
-  
   namespace :drush do
 
     desc "Backup the database"
